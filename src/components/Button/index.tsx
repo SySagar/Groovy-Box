6d6d5de408a5c14f6e1208c1ce7f1e213a19860c
SySagar <sysagar07@gmail.com>
2023-11-08 23:40:46 +0530

    chore:version update

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,19 +2,22 @@ import React from 'react';
 
 type ButtonVariant = 'filled' | 'outlined' | 'text';
 type ButtonSize = 'sm' | 'md' | 'lg';
-type ButtonColor = 'primary' | 'secondary' | 'danger' | 'success' | 'warning' | 'info';
+type ButtonColor = 'primary' | 'secondary';
 
 export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement>{
     variant?: ButtonVariant;
     size?: ButtonSize;
     color?: ButtonColor;
+
 }
 
 export const Button: React.FC<ButtonProps> = (
     {variant = 'filled', 
     size = 'md',
      color = 'primary',
-      children, ...props}) => {
+      children,
+      ...props
+    }) => {
 
         const baseStyles =
         'rounded font-semibold focus:outline-none focus:ring-2 focus:ring-offset-2';
@@ -30,11 +33,13 @@ export const Button: React.FC<ButtonProps> = (
           filled: 'bg-[#35FF1F] hover:bg-[#00C200] text-white focus:ring-[#00C200]',
           outlined:
             'bg-transparent hover:bg-[#dcfce7] text-[#35FF1F] border-[#35FF1F] border focus:ring-[#00C200]',
+          text: 'bg-transparent hover:bg-[#dcfce7] text-[#35FF1F] focus:ring-[#00C200]',
         },
         secondary: {
           filled: 'bg-gray-500 hover:bg-gray-600 text-white focus:ring-gray-400',
           outlined:
             'bg-transparent hover:bg-gray-100 text-gray-500 border-gray-500 border focus:ring-gray-400',
+          text: 'bg-transparent hover:bg-gray-100 text-gray-500 focus:ring-gray-400',
         },
       };
     
