17e81d609df2ee9f8e7cb1863c82c0f6a7115042
SySagar <sysagar07@gmail.com>
2024-02-17 01:33:43 +0530

    feat: typography

---
+++ tokens.ts
@@ -0,0 +1,102 @@
+const customClasses = {
+  '.typo-body-1': {
+    fontSize: '16px',
+    lineHeight: '24px',
+    fontWeight: '400',
+  },
+  '.typo-body-2': {
+    fontSize: '14px',
+    lineHeight: '20px',
+    fontWeight: '400',
+  },
+  '.typo-body-3': {
+    fontSize: '12px',
+    lineHeight: '16px',
+    fontWeight: '400',
+  },
+  '.typo-button-1': {
+    fontSize: '16px',
+    lineHeight: '26px',
+    fontWeight: '500',
+    letterSpacing: '0.1px',
+  },
+  '.typo-button-2': {
+    fontSize: '14px',
+    lineHeight: '20px',
+    fontWeight: '500',
+  },
+  '.typo-button-3': {
+    fontSize: '12px',
+    lineHeight: '16px',
+    fontWeight: '500',
+  },
+  '.typo-heading-1': {
+    fontSize: '28px',
+    lineHeight: '36px',
+    fontWeight: '400',
+  },
+  '.typo-heading-2': {
+    fontSize: '24px',
+    lineHeight: '30px',
+    fontWeight: '600',
+  },
+  '.typo-heading-3': {
+    fontSize: '20px',
+    lineHeight: '26px',
+    fontWeight: '600',
+  },
+  '.typo-heading-4': {
+    fontSize: '18px',
+    lineHeight: '24px',
+    fontWeight: '600',
+  },
+  '.typo-heading-5': {
+    fontSize: '16px',
+    lineHeight: '22px',
+    fontWeight: '600',
+  },
+  '.typo-heading-6': {
+    fontSize: '14px',
+    lineHeight: '20px',
+    fontWeight: '600',
+  },
+  '.typo-label-1': {
+    fontSize: '14px',
+    lineHeight: '20px',
+    fontWeight: '400',
+    letterSpacing: '-0.24px',
+  },
+  '.typo-label-2': {
+    fontSize: '12px',
+    lineHeight: '16px',
+    fontWeight: '400',
+  },
+  '.typo-label-2-strong': {
+    fontSize: '12px',
+    lineHeight: '16px',
+    fontWeight: '500',
+  },
+  '.typo-label-3': {
+    fontSize: '10px',
+    lineHeight: '12px',
+    fontWeight: '400',
+  },
+  '.typo-subtitle-1': {
+    fontSize: '16px',
+    lineHeight: '22px',
+    fontWeight: '500',
+    letterSpacing: '0.1px',
+  },
+  '.typo-subtitle-2': {
+    fontSize: '14px',
+    lineHeight: '20px',
+    fontWeight: '500',
+  },
+  '.typo-subtitle-3': {
+    fontSize: '12px',
+    lineHeight: '16px',
+    fontWeight: '500',
+  },
+};
+
+export default customClasses;
