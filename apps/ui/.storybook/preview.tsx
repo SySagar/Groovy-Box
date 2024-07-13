6b2321b4a50d1f05697823e76035fa1a716033a1
Sagar <sysagar07@gmail.com>
2024-07-14 00:56:36 +0530

    fix: react-vite storybook setup

---
+++ apps/ui/.storybook/preview.tsx
@@ -0,0 +1,47 @@
+// https://storybook.js.org/docs/react/writing-stories/parameters#global-parameters
+
+const customViewports = {
+  xs: {
+    name: 'XS',
+    styles: {
+      width: '350px',
+      height: '963px',
+    },
+  },
+  sm: {
+    name: 'SM',
+    styles: {
+      width: '600px',
+      height: '801px',
+    },
+  },
+  md: {
+    name: 'MD',
+    styles: {
+      width: '900px',
+      height: '801px',
+    },
+  },
+  lg: {
+    name: 'LG',
+    styles: {
+      width: '1200px',
+      height: '801px',
+    },
+  },
+  xl: {
+    name: 'XL',
+    styles: {
+      width: '1536px',
+      height: '801px',
+    },
+  },
+};
+
+export const parameters = {
+  // https://storybook.js.org/docs/react/essentials/actions#automatically-matching-args
+  actions: { argTypesRegex: '^on.*' },
+  visualViewport: {
+    customViewports,
+  },
+};
