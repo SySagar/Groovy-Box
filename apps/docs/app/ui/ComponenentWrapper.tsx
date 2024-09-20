f99297f48085cfedc6254847ab3f94d4d1decf5c
SySagar <sysagar07@gmail.com>
2024-09-20 17:30:00 +0530

    docs(button): basic button variants and sizes

---
+++ apps/docs/app/ui/ComponenentWrapper.tsx
@@ -0,0 +1,14 @@
+'use client';
+import React from 'react';
+
+export default function ComponenentWrapper({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className='flex flex-row items-center justify-center h-[300px] gap-20'>
+      {children}
+    </div>
+  );
+}
