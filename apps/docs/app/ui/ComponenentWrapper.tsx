68ceb083cc13e22982d308bcdd5a73a1dc389992
SySagar <sysagar07@gmail.com>
2024-10-04 23:04:33 +0530

    chore(docs): tailwind-merge classes

--- apps/docs/app/ui/ComponenentWrapper.tsx
+++ apps/docs/app/ui/ComponenentWrapper.tsx
@@ -1,18 +1,21 @@
 'use client';
 import React from 'react';
+import { cn } from '../utils/utils';
+
+interface ComponentWrapperProps extends React.HTMLAttributes<HTMLDivElement> {
+  children: React.ReactNode;
+  height?: string;
+}
 
 export default function ComponenentWrapper({
   children,
   height = '300px',
-}: {
-  children: React.ReactNode;
-  height?: string;
-}) {
-  return (
-    <div
-      className={`ui flex flex-row items-center justify-center h-[${height}] gap-20`}
-    >
-      {children}
-    </div>
+  ...props
+}: ComponentWrapperProps) {
+  console.log('hetight', height);
+  const mergedClassNames = cn(
+    `ui flex flex-row items-center justify-center h-[${height}] gap-20`,
+    props.className,
   );
+  return <div className={mergedClassNames}>{children}</div>;
 }
